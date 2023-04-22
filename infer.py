c0769d47e71994c31d77bdeb11730df21772db6c
Ivan Kremnev <iakremnev@mail.ru>
2023-04-23 00:08:34 +0300

    write video stream on disk

--- infer.py
+++ infer.py
@@ -35,26 +35,35 @@ def infer_image(inferencer: OpenVINOInferencer, image_path: str, vis_all_predict
         cv2.imwrite(save_to, output)
 
 
-def infer_camera_stream(inferencer: OpenVINOInferencer) -> None:
+def infer_camera_stream(inferencer: OpenVINOInferencer, out_video_file: str) -> None:
     cap = cv2.VideoCapture(0)
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    out_cap = cv2.VideoWriter(out_video_file, cv2.VideoWriter_fourcc("M", "J", "P", "G"), 15, (frame_width, frame_height))
     
-    while True:
-        success, frame = cap.read()
+    try:
+        while cap.isOpened():
+            success, frame = cap.read()
 
-        if success:
-            predictions = inferencer.predict(frame)
-            output = predictions.segmentations
-            text_col = (0, 255, 0) if predictions.pred_label else (0, 0, 255)
-            cv2.putText(output, f"Anom={predictions.pred_label}\nScore={predictions.pred_score:.4f}", (5, 5), cv2.FONT_HERSHEY_PLAIN, 2, text_col, 2)
-            cv2.imshow("Camera inference", output)
+            if success:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                predictions = inferencer.predict(frame)
+                output = predictions.segmentations
+                print(i, predictions.pred_score)
+                text_col = (255, 0, 0) if predictions.pred_label else (255, 0, 0)
+                cv2.putText(output, f"Anom={predictions.pred_label}", (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, text_col, 2)
+                cv2.putText(output, f"Score={predictions.pred_score:.4f}", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, text_col, 2)
+                # cv2.imshow("Camera_inference", output)
+                output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
+                out_cap.write(output)
 
-            if cv2.waitKey(1) & 0xFF == ord("q"):
+            else:
                 break
-
-        else:
-            break
+    except KeyboardInterrupt:
+        print("Stopping video stream")
     
     cap.release()
+    out_cap.release()
     cv2.destroyAllWindows()
 
 
@@ -77,7 +86,9 @@ if __name__ == "__main__":
     )
 
     if args.camera:
-        infer_camera_stream(inferencer)
+        # For some unfortunate reason, cv2.imshow freezes when the OpenVINOInferencer is imported
+        # so the video must be recorded on disk
+        infer_camera_stream(inferencer, out_video_file="out.avi")
     else:
         image_paths = args.image
         os.makedirs("predictions", exist_ok=True)
