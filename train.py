05d670d696dd9145b5bc812d1d1d110a0c14a6dc
Ivan Kremnev <iakremnev@mail.ru>
2023-04-22 17:58:28 +0300

    padim trainig script

---
+++ train.py
@@ -0,0 +1,33 @@
+import os.path
+
+from pytorch_lightning import Trainer
+
+from anomalib.config import get_configurable_parameters
+from anomalib.data import get_datamodule
+from anomalib.models import get_model
+from anomalib.utils.callbacks import LoadModelCallback, get_callbacks
+
+
+CONFIG_PATH = os.path.join("models", "padim", "config.yaml")
+
+config = get_configurable_parameters(CONFIG_PATH)
+
+
+datamodule = get_datamodule(config)
+datamodule.prepare_data()  # Downloads the dataset if it's not in the specified `root` directory
+datamodule.setup()  # Create train/val/test/prediction sets.
+
+# Set the export-mode to OpenVINO to create the OpenVINO IR model.
+config.optimization.export_mode = "openvino"
+
+model = get_model(config)
+callbacks = get_callbacks(config)
+
+# start training
+trainer = Trainer(**config.trainer, callbacks=callbacks)
+trainer.fit(model=model, datamodule=datamodule)
+
+# load best model from checkpoint before evaluating
+load_model_callback = LoadModelCallback(weights_path=trainer.checkpoint_callback.best_model_path)
+trainer.callbacks.insert(0, load_model_callback)
+test_results = trainer.test(model=model, datamodule=datamodule)
